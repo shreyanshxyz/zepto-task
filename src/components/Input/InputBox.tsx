febb3e0991ef8268c6d59ecb253a283e8c20094d
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 15:19:07 +0530

    update InputBox.tsx

--- src/components/Input/InputBox.tsx
+++ src/components/Input/InputBox.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 
-type InputBoxProps = {};
+type InputBoxProps = {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick: () => void;
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  children?: ReactNode;
+};
 
-const InputBox: React.FC<InputBoxProps> = () => {
+const InputBox: React.FC<InputBoxProps> = ({
+  value,
+  onChange,
+  onClick,
+  onKeyDown,
+  children,
+}) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
   return (
     <div>
       <input
         className="text-sm p-3 pt-10 border-blue-800 border-b-[3px] min-w-[800px] outline-none"
         type="text"
+        onChange={onChange}
+        onClick={onClick}
+        onKeyDown={onKeyDown}
+        value={value}
+        ref={inputRef}
         placeholder="Add new user..."
       />
     </div>
