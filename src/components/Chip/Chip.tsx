d21501f9da7e3e8e03c82b68e8e0e88dd08d8b2c
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 19:52:07 +0530

    update Chip.tsx

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import Avatar from "../Avatar/Avatar";
 
-type ChipProps = {};
+type ChipData = {
+  id: number;
+  name: string;
+  avatar: string;
+  email: string;
+};
 
-const Chip: React.FC<ChipProps> = () => {
+type ChipProps = {
+  item: ChipData;
+  highlightedItem: ChipData | null | undefined;
+  onRemove: () => void;
+};
+
+const Chip: React.FC<ChipProps> = ({ item, onRemove, highlightedItem }) => {
   return (
-    <div className="flex justify-between items-center bg-[#e0e0e0] p-2 rounded-3xl mr-2 border border-white border-solid">
-      <Avatar />
-      <p className="mr-4">Jonathan Doe</p>
-      <button className="cursor-pointer h-5 w-5 text-[rgb(49,48,48)] flex items-center justify-center rounded-[50%] border-[none]">
-        X
+    <div
+      className={
+        highlightedItem?.id === item.id
+          ? "flex justify-between items-center bg-[#e0e0e0] px-3 py-1 mb-2 rounded-3xl mr-2 border border-solid border-orange-400"
+          : "flex justify-between items-center bg-[#e0e0e0] px-3 py-1 mb-2 rounded-3xl mr-2 border border-solid border-white"
+      }
+    >
+      <Avatar avatarUrl={item.avatar} altText={item.name} />
+      <p className="mr-2">{item.name}</p>
+      <button
+        className="flex items-center justify-center cursor-pointer h-5 w-5 border-none rounded-full font-black text-black"
+        onClick={onRemove}
+      >
+        &#x2715;
       </button>
     </div>
   );
