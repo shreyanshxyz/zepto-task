60cb869e639f72c9a493450949a05ec5fd379e83
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 15:19:13 +0530

    update DropdownModal.tsx

--- src/components/Modal/DropdownModal.tsx
+++ src/components/Modal/DropdownModal.tsx
@@ -1,8 +1,47 @@
 import React from "react";
+import Avatar from "../Avatar/Avatar";
 
 type DropdownModalProps = {};
 
 const DropdownModal: React.FC<DropdownModalProps> = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <div className="modal">
+      <div className="item">
+        <Avatar />
+        <div className="ml-2">
+          <span>Jonathan Doe</span>
+          <span>johndoe@gmail.com</span>
+        </div>
+      </div>
+      <div className="item">
+        <Avatar />
+        <div className="ml-2">
+          <span>Jonathan Doe</span>
+          <span>johndoe@gmail.com</span>
+        </div>
+      </div>
+      <div className="item">
+        <Avatar />
+        <div className="ml-2">
+          <span>Jonathan Doe</span>
+          <span>johndoe@gmail.com</span>
+        </div>
+      </div>
+      <div className="item">
+        <Avatar />
+        <div className="ml-2">
+          <span>Jonathan Doe</span>
+          <span>johndoe@gmail.com</span>
+        </div>
+      </div>
+      <div className="item">
+        <Avatar />
+        <div className="ml-2">
+          <span>Jonathan Doe</span>
+          <span>johndoe@gmail.com</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default DropdownModal;
