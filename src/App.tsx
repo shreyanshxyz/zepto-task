13c90972f6661d6bd6c9ffc52f4e73215b83a0c4
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 13:41:08 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import Chip from "./components/Chip/Chip";
+import InputBox from "./components/Input/InputBox";
 
 function App() {
-  return <div className="App">Hello, React + TS</div>;
+  return (
+    <div className="text-center flex flex-col">
+      <h1 className="text-3xl pt-10 text-blue-800">Pick Users</h1>
+      <InputBox />
+      <Chip />
+      <Chip />
+      <Chip />
+      <Chip />
+      <Chip />
+    </div>
+  );
 }
 
 export default App;
