6116c5ca8276cb514c63d667de8a123e6a644f70
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 13:41:21 +0530

    feat: create DropdownModal.tsx

---
+++ src/components/Modal/DropdownModal.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type DropdownModalProps = {};
+
+const DropdownModal: React.FC<DropdownModalProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default DropdownModal;
