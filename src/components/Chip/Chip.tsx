3247c3fe3dd080c606289bd779b95c9a89640712
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 20:48:18 +0530

    update Chip.tsx

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -19,14 +19,14 @@ const Chip: React.FC<ChipProps> = ({ item, onRemove, highlightedChip }) => {
     <div
       className={
         highlightedChip?.id === item.id
-          ? "flex justify-between items-center bg-[#e0e0e0] p-1 mb-2 rounded-3xl mr-2 border border-solid border-blue-600 gap-2"
-          : "flex justify-between items-center bg-[#e0e0e0] p-1 mb-2 rounded-3xl mr-2 border border-solid border-white gap-2 "
+          ? "flex justify-between items-center bg-[#e0e0e0] p-1 mb-2 rounded-3xl mr-2 border border-solid border-blue-600 gap-2 max-sm:mb-1"
+          : "flex justify-between items-center bg-[#e0e0e0] p-1 mb-2 rounded-3xl mr-2 border border-solid border-white gap-2 max-sm:mb-1"
       }
     >
       <Avatar avatarUrl={item.avatar} altText={item.name} />
-      <span className="font-serif">{item.name}</span>
+      <span className="font-serif max-sm:text-sm">{item.name}</span>
       <button
-        className="flex items-center justify-center cursor-pointer h-5 w-5 border-none rounded-full font-black text-black"
+        className="flex items-center justify-center cursor-pointer h-5 w-5 border-none rounded-full text-black font-black"
         onClick={onRemove}
       >
         &#x2715;
