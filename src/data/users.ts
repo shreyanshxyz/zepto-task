2e0064574e736b22a165915f3d5316309c6e019c
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 21:05:32 +0530

    feat: create users.ts - user dummy data

---
+++ src/data/users.ts
@@ -0,0 +1,66 @@
+import ChipData from "../types/ChipData";
+
+const users: ChipData[] = [
+  {
+    id: 1,
+    name: "John Smith",
+    email: "john.smith@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567590",
+  },
+  {
+    id: 2,
+    name: "Anna Johnson",
+    email: "anna.johnson@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567591",
+  },
+  {
+    id: 3,
+    name: "Charlie Davis",
+    email: "charlie.davis@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567592",
+  },
+  {
+    id: 4,
+    name: "David Miller",
+    email: "david.miller@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567593",
+  },
+  {
+    id: 5,
+    name: "Ella Robinson",
+    email: "ella.robinson@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567594",
+  },
+  {
+    id: 6,
+    name: "Frank Taylor",
+    email: "frank.taylor@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567595",
+  },
+  {
+    id: 7,
+    name: "Gina Martinez",
+    email: "gina.martinez@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567596",
+  },
+  {
+    id: 8,
+    name: "Henry Clark",
+    email: "henry.clark@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567597",
+  },
+  {
+    id: 9,
+    name: "Katie Turner",
+    email: "katie.turner@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567598",
+  },
+  {
+    id: 10,
+    name: "Leo Mitchell",
+    email: "leo.mitchell@gmail.com",
+    avatar: "https://avatars.githubusercontent.com/u/9567500",
+  },
+];
+
+export default users;
