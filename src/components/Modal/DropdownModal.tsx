f5984fe37f5d8fa2a76925820f2f00dc472b4454
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 19:52:15 +0530

    update DropdownModal.tsx

--- src/components/Modal/DropdownModal.tsx
+++ src/components/Modal/DropdownModal.tsx
@@ -1,46 +1,43 @@
 import React from "react";
 import Avatar from "../Avatar/Avatar";
 
-type DropdownModalProps = {};
+type ChipData = {
+  id: number;
+  name: string;
+  avatar: string;
+  email: string;
+};
 
-const DropdownModal: React.FC<DropdownModalProps> = () => {
+type DropdownModalProps = {
+  items: ChipData[];
+  inputValue: string;
+  onItemClick: (item: ChipData) => void;
+};
+
+const DropdownModal: React.FC<DropdownModalProps> = ({
+  items,
+  inputValue,
+  onItemClick,
+}) => {
   return (
-    <div className="modal">
-      <div className="item">
-        <Avatar />
-        <div className="ml-2">
-          <span>Jonathan Doe</span>
-          <span>johndoe@gmail.com</span>
-        </div>
-      </div>
-      <div className="item">
-        <Avatar />
-        <div className="ml-2">
-          <span>Jonathan Doe</span>
-          <span>johndoe@gmail.com</span>
-        </div>
-      </div>
-      <div className="item">
-        <Avatar />
-        <div className="ml-2">
-          <span>Jonathan Doe</span>
-          <span>johndoe@gmail.com</span>
-        </div>
-      </div>
-      <div className="item">
-        <Avatar />
-        <div className="ml-2">
-          <span>Jonathan Doe</span>
-          <span>johndoe@gmail.com</span>
-        </div>
-      </div>
-      <div className="item">
-        <Avatar />
-        <div className="ml-2">
-          <span>Jonathan Doe</span>
-          <span>johndoe@gmail.com</span>
-        </div>
-      </div>
+    <div className="absolute top-[50px] left-0 bg-white rounded-lg shadow-2xl z-[1] overflow-y-auto scrollbar max-h-[450px]">
+      {items
+        .filter((item) =>
+          item.name.toLowerCase().includes(inputValue.toLowerCase())
+        )
+        .map((item) => (
+          <div
+            key={item.id}
+            className="cursor-pointer p-3 flex justify-between  gap-4 items-center hover:bg-[#f0f0f0] min-w-[450px]"
+            onClick={() => onItemClick(item)}
+          >
+            <div className="flex items-center gap-4">
+              <Avatar avatarUrl={item.avatar} altText={item.name} />
+              <h5> {item.name}</h5>
+            </div>
+            <p>{item.email}</p>
+          </div>
+        ))}
     </div>
   );
 };
