bcb021c73c88c9a2837cb2e348920aabddcbe68b
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 20:48:13 +0530

    update Avatar.tsx

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -7,7 +7,11 @@ type AvatarProps = {
 
 const Avatar: React.FC<AvatarProps> = ({ avatarUrl, altText }) => {
   return (
-    <img className="w-10 h-10 rounded-full" src={avatarUrl} alt={altText} />
+    <img
+      className="w-10 h-10 rounded-full max-sm:w-5 max-sm:h-5"
+      src={avatarUrl}
+      alt={altText}
+    />
   );
 };
 export default Avatar;
