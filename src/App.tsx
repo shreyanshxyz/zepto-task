7b4a902890e184e997cec06e04d79b0b8d7240c3
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2024-01-16 15:19:10 +0530

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import Chip from "./components/Chip/Chip";
+import { useState } from "react";
 import InputBox from "./components/Input/InputBox";
+import DropdownModal from "./components/Modal/DropdownModal";
 
 function App() {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
+
+  function toggleModal() {
+    setShowModal((value) => !value);
+    console.log(showModal);
+  }
+
+  function handleKeyPress() {}
+
   return (
     <div className="text-center flex flex-col">
       <h1 className="text-3xl pt-10 text-blue-800">Pick Users</h1>
-      <InputBox />
-      <Chip />
-      <Chip />
-      <Chip />
-      <Chip />
-      <Chip />
+      <InputBox
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+        onClick={toggleModal}
+      />
+      {showModal && <DropdownModal />}
     </div>
   );
 }
